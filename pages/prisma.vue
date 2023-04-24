dcabd451388f75673a1f4319a96311cb486e7cb7
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-24 22:36:43 +0300

    trying things to deploy to railway

--- pages/prisma.vue
+++ pages/prisma.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-const { data: examples } = useFetch('/api/examples')
+const {$client} = useNuxtApp()
+
+const {data: examples} = await $client.prisma.useQuery()
 </script>
 
 <template>
   <div>
-    Prisma ORM Data from the database, received {{ examples?.length || 0 }} records: <pre>{{ examples }}</pre>
+    Prisma ORM Data from the database, received {{ examples?.length || 0 }} records:
+    <pre>{{ examples }}</pre>
   </div>
 </template>
