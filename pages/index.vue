e5c99311f0ca0dd96eac2122452eb8d33ab0ae21
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-25 16:28:14 +0300

    Added nuxt-typed-router

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>{{ hello }}</div>
+  <div>Home Page</div>
 </template>
 
 <script setup lang="ts">
-const {$client} = useNuxtApp()
+// const {$client} = useNuxtApp()
 
-const hello = await $client.hello.useQuery({text: 'Hello World!'})
+// const hello = await $client.hello.useQuery({text: 'Hello World!'})
 </script>
