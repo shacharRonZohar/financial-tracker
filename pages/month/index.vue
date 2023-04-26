4566c20d02e6589a4626fbf3bcdfcd8e31d8aab5
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-26 20:27:24 +0300

    related to last commit

--- pages/month/index.vue
+++ pages/month/index.vue
@@ -2,17 +2,11 @@
   <main>
     <YearSelector />
     <MonthSelector />
-    <MonthOverview :year="year" :month-num="month" />
+    <MonthOverview />
   </main>
 </template>
 
 <script setup lang="ts">
 import YearSelector from '~/components/YearSelector.vue'
 import MonthSelector from '~/components/MonthSelector.vue'
-
-const route = useRoute('month')
-
-const year = computed(() => +(route.query?.year || new Date().getFullYear()))
-
-const month = computed(() => +(route.query?.monthNum || new Date().getMonth() + 1))
 </script>
