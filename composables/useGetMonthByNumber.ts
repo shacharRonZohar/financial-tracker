99cb21b8d351a026cc11b1cb28be1c7fd2ea496c
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-26 21:26:54 +0300

    refactored useGetMonthByNumber to accept queryKey from outside

--- composables/useGetMonthByNumber.ts
+++ composables/useGetMonthByNumber.ts
@@ -1,20 +1,28 @@
-import {useQuery} from '@tanstack/vue-query'
+import {UseMutationOptions, useQuery, useQueryClient} from '@tanstack/vue-query'
 import type {MonthGetByNumberInput} from '~/models/trpc'
 import type {MapToMaybeRefInputs, MaybeRef} from '~/models/utils'
 
-type UseGetMonthByNumberOptions = MapToMaybeRefInputs<MonthGetByNumberInput>
+type UseGetMonthByNumberOptions = MapToMaybeRefInputs<MonthGetByNumberInput> & {
+  queryKey: Ref<any[]>
+}
 
-export function useGetMonthByNumber({yearInput, monthNumInput}: UseGetMonthByNumberOptions) {
+export function useGetMonthByNumber({queryKey, yearInput, monthNumInput}: UseGetMonthByNumberOptions) {
   const {$client} = useNuxtApp()
+
   const year = ref(yearInput)
   const monthNum = ref(monthNumInput)
 
-  const queryKey = ['month', 'getByNumber', {year: year.value, monthNum: monthNum.value}]
   const queryFn = () => $client.month.getByNumber.query({year: year.value, monthNum: monthNum.value})
 
-  const {data: month, error: monthError, isLoading: isLoadingMonth, refetch: refetchMonth} = useQuery(queryKey, queryFn)
+  const {
+    data: month,
+    error: monthError,
+    isLoading: isLoadingMonth,
+    refetch: refetchMonth,
+  } = useQuery(queryKey.value, queryFn)
 
   return {
+    queryKey,
     month,
     monthError,
     isLoadingMonth,
