ce54964eb6bd81c04ac7d64471dce572cf97a3ac
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-29 23:25:00 +0300

    added a CreateExpenseModal to ease the use of that modal

---
+++ components/CreateExpenseModal.vue
@@ -0,0 +1,32 @@
+<template>
+    <GenericFormModal :fields="fields" @submit="handleSubmit"></GenericFormModal>
+</template>
+
+<script setup lang="ts">
+import z from 'zod'
+const fields = [
+    {
+        label: 'Name',
+        name: 'name',
+        placeholder: 'Name',
+        type: 'text',
+        validation: z.string().min(1)
+    },
+    {
+        name: 'amount',
+        type: 'number',
+        label: 'Amount',
+        placeholder: 'Amount',
+        validation: z.number().min(1)
+    },
+] as const
+
+const emit = defineEmits<{
+    (event: 'submit', formData: { amount: number, name: string }): void
+}>()
+
+function handleSubmit({ amount, name }: Record<string, any>) {
+    emit('submit', { amount, name })
+}
+
+</script>
