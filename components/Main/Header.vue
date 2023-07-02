3118e18f4253da2ca11f4d3dfdc8f452585549aa
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-07-02 19:50:08 +0300

    feat: add navbar

--- components/Main/Header.vue
+++ components/Main/Header.vue
@@ -1,5 +1,22 @@
 <template>
   <header>
     <h1>Header</h1>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link :to="`/overview/`">Overview</nuxt-link></li>
+      </ul>
+    </nav>
   </header>
 </template>
+<!--
+<script setup lang="ts">
+const route = useRoute()
+console.log(route)
+const router = useRouter()
+console.log(router)
+
+function goToOverview() {
+  router.push('/overview/')
+}
+</script> -->
