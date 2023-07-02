b6c649fb07069ce7aea3fea8dc023a926fcdce5f
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-07-02 19:57:12 +0300

    feat: add scss & main layout

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="">
+  <div class="app-container">
     <MainHeader />
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+.app-container {
+  display: grid;
+  grid-template-columns: minmax(20px, 1fr) minmax(200px, 6fr) minmax(20px, 1fr);
+
+  > * {
+    grid-column: 2;
+  }
+}
+</style>
